c89a3eaf2500ac26a8b20552b1f849ed2acf318c
samehjadda <samehjadda@gmail.com>
2023-05-25 23:09:42 -0400

    bug fixes

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { WeatherData } from '../models/weather.model';
 import { Observable } from 'rxjs';
 
@@ -13,7 +12,7 @@ export class WeatherService {
 
   getWeatherData(cityName: string): Observable<WeatherData> {
     const api_key = '8e60f918b8c44268982204033232505';
-    const weatherApiBaseUrl = 'http://api.weatherapi.com/v1';
+    const weatherApiBaseUrl = 'https://api.weatherapi.com/v1';
     const url = `${weatherApiBaseUrl}/current.json?key=${api_key}&q=${cityName}`;
     
     return this.http.get<WeatherData>(url);
